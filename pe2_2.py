32fa8db712bfe6577cee2b0db456463a922f8276
Martin Vlaanderen <martin.vlaanderen@student.hu.nl>
2017-09-13 16:17:24 +0200

    opdracht 2 van h2

---
+++ pe2_2.py
@@ -0,0 +1,16 @@
+cijferICOR=6
+cijferPROG=5
+cijferCSN=4
+
+gemiddelde=(6+5+4)/3
+beloning=(30*6)+(30*5)+(30*4)
+
+overzicht='Mijn cijfers leveren gemiddeld een 5,0 en een beloning van 450 euro op'
+
+
+
+
+
+
+
+
